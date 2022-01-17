151f8500ff74fba9b91974b3a32a72e590d33f77
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-01-17 12:19:49 +0000

    bridge plugin start

---
+++ bridge/hello.php
@@ -0,0 +1,47 @@
+<?php
+
+function hello_world() {
+	$chosen = wptexturize("Hello World");
+	$lang   = '';
+	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
+		$lang = ' lang="en"';
+	}
+
+	printf(
+		'<p id="dolly"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
+		__( 'Quote from Hello Dolly song, by Jerry Herman:' ),
+		$lang,
+		$chosen
+	);
+}
+
+function hello_world_css() {
+	echo "
+	<style type='text/css'>
+	#dolly {
+		float: right;
+		padding: 5px 10px;
+		margin: 0;
+		font-size: 12px;
+		line-height: 1.6666;
+	}
+	.rtl #dolly {
+		float: left;
+	}
+	.block-editor-page #dolly {
+		display: none;
+	}
+	@media screen and (max-width: 782px) {
+		#dolly,
+		.rtl #dolly {
+			float: none;
+			padding-left: 0;
+			padding-right: 0;
+		}
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_notices', 'hello_world' );
+add_action( 'admin_head', 'hello_world_css' );
--- bridge/index.php
+++ bridge/index.php
@@ -1,2 +1,13 @@
 <?php
-// Bridge Plugin
+/**
+ * @package Bridge Plugin
+ * @version 0.0.1
+ */
+/*
+Plugin Name: Bridge
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: Bridge Plugin by Platforme MTO.
+Author: Platforme MTO
+Version: 0.0.1
+Author URI: https://www.platforme.com/
+*/
