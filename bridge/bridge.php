e5242b721b75868753d6bf6e285b26fd736f05b0
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-01-21 13:31:31 +0000

    worst php ever written, not that php could ever be nice

--- bridge/bridge.php
+++ bridge/bridge.php
@@ -1,16 +1,41 @@
 <?php
+/**
+ * @package Bridge Plugin
+ * @version 0.0.1
+ */
+/*
+Plugin Name: Bridge Plugin
+Plugin URI: https://github.com/ripe-tech/ripe-bridge
+Description: Bridge Plugin by Platforme MTO.
+Author: Platforme MTO
+Version: 0.0.1
+Author URI: https://github.com/ripe-tech
+*/
+defined("ABSPATH") or die;
 
-function checkout_order_created($order) {
-	var_dump($order);
-	die("checkout_order_created");
+class BridgePlugin {
+	function activate() {
+		echo "The plugin was activated";
+	}
+
+	function deactivate() {
+		echo "The plugin was deactivated";
+	}
+
+	function uninstall() {
+
+	}
 }
 
-function checkout_order_processed($order_id, $posted_data, $order) {
-	var_dump($order);
-	die("checkout_order_processed");
+function add_customize_button($add_to_cart_button) {
+	echo $add_to_cart_button;
+	return "<button type='button'>Customize</button>";
 }
 
-add_action('woocommerce_checkout_order_created', 'checkout_order_created', 10, 1);
-add_action('woocommerce_checkout_order_processed', 'checkout_order_processed', 10, 3);
+if (class_exists("BridgePlugin")) $bridge_plugin = new BridgePlugin();
 
-?>
+register_activation_hook(__FILE__, array($bridge_plugin, "activate"));
+
+register_deactivation_hook(__FILE__, array($bridge_plugin, "deactivate"));
+
+add_action("woocommerce_loop_add_to_cart_link", 'add_customize_button', 10, 1);
--- bridge/index.php
+++ bridge/index.php
@@ -1,14 +1,2 @@
 <?php
-    /**
-     * @package Bridge Plugin
-     * @version 0.0.1
-     */
-    /*
-    Plugin Name: Bridge Plugin
-    Plugin URI: https://github.com/ripe-tech/ripe-bridge
-    Description: Bridge Plugin by Platforme MTO.
-    Author: Platforme MTO
-    Version: 0.0.1
-    Author URI: https://github.com/ripe-tech
-    */
-?>
+// Silence is golden.
