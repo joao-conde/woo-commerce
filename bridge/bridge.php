386f2900670bee06a28bf28887bfac2e43514764
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-01-21 12:04:58 +0000

    ...

--- bridge/bridge.php
+++ bridge/bridge.php
@@ -1,10 +1,4 @@
 <?php
-if (!defined('ABSPATH')) exit;
-
-die("THIS RUNS")
-
-add_action('woocommerce_checkout_order_created', 'checkout_order_created', 10, 1);
-add_action('woocommerce_checkout_order_processed', 'checkout_order_processed', 10, 3);
 
 function checkout_order_created($order) {
 	var_dump($order);
@@ -16,11 +10,7 @@ function checkout_order_processed($order_id, $posted_data, $order) {
 	die("checkout_order_processed");
 }
 
-
-add_action('woocommerce_proceed_to_checkout', 'dummy', 10, 1);
-function dummy($order) {
-	var_dump($order);
-	die("woocommerce_proceed_to_checkout");
-}
+add_action('woocommerce_checkout_order_created', 'checkout_order_created', 10, 1);
+add_action('woocommerce_checkout_order_processed', 'checkout_order_processed', 10, 3);
 
 ?>
